b570cc609c63754bf32896ec2b1b603f702ed6ab
Tom Totenberg <ttotenberg@launchdarkly.com>
2025-01-21 13:23:03 -0600

    updated for mh

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 import { deviceType, osName } from "react-device-detect";
 import getUserId from "./util/getUserId";
 
-const CLIENTKEY = "609ead905193530d7c28647b";
+const CLIENTKEY = "678ff1f80d9c77097efd21c9";
 
 let id = getUserId();
 
